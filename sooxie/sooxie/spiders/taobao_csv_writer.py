0d720a1bd3dbcedb896118e4f5ac3b7a4d771dfb
yanzl <51877421@qq.com>
2017-04-19 19:04:47 +0800

    属性分析

--- sooxie/sooxie/spiders/taobao_csv_writer.py
+++ sooxie/sooxie/spiders/taobao_csv_writer.py
@@ -847,7 +847,9 @@ def _add_row(rowsitem):
         csvfile.close()
         print "写入结束"
 
-def operator_mainimg(mainimg):
+
+def get_cid(sooxie):
+    # 返加宝贝类目
 
     return ""
 
@@ -904,8 +906,30 @@ def add_row(sooxie):
     # 添加一行数据
     rowsitem = Rowsitem()
     rowsitem.title = sooxie.title;
-    
-    _add_row(rowsitem)
+    rowsitem.cid = get_cid(sooxie);  # 宝贝类目
+    rowsitem.seller_cids = "";  # 店铺类目固定
+
+
+
+
+
+
+
+    def __init__(self):
+        self.url = None  # 链接地址
+        self.title = None  # 标题
+        self.mainimg = None  # 主图列表
+        self.shoeno = None  # 货号
+        self.price = None  # 价格
+        self.popularity = None  # 人气
+        self.update = None  # 更新时间
+        self.sizes = None  # 尺码
+        self.colors = None  # 颜色
+        self.images = None  # 图片链接地址
+        self.property = None  # 属性
+        self.market = None  # 市场
+
+    # _add_row(rowsitem)
 
     # self.url = None  # 链接地址
     # self.title = None  # 标题
